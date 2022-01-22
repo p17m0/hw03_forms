ced5ab8c71fe7042ebd70bc4a36d7065836e2353
Matvey Dorofeev <matiaso@bk.ru>
2022-01-22 21:37:09 +0300

    WOW

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -68,8 +68,6 @@ def post_edit(request, post_id):
     form = PostForm(request.POST or None, instance=post)
     if post.author == request.user and form.is_valid():
         form.save()
-        post.author = request.user
-        post.save()
         return redirect('posts:post_detail', post_id)
     return render(request, 'posts/create_post.html',
                   {'is_edit': True, 'form': form, })
